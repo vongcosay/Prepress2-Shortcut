225a5fae77631131614c32fdfe0ae16330b0496f
Do Nguyen <vongcosay@gmail.com>
2026-01-31 04:39:31 +0700

    Update Prepress Shortcut.ps1

--- Prepress Shortcut.ps1
+++ Prepress Shortcut.ps1
@@ -1,1 +1,5 @@
 powershell.exe -windowstyle hidden -command "$path = Get-Clipboard; if (Test-Path $path) { explorer.exe /select,$path } else { msg * 'Duong dan khong hop le!' }"
+
+
+powershell.exe -windowstyle hidden -command "$path = (Get-Clipboard).Trim('\" '); if (Test-Path $path) { explorer.exe /select,$path } else { msg * 'Duong dan khong hop le!' }"
+
